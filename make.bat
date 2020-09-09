6b84bef161516a1f5ab14ec16806480d2904bf52
sevenc-nanashi <sevenc_nanashi@yahoo.co.jp>
2020-09-09 16:12:24 +0900

    ライセンス等を追加

--- make.bat
+++ make.bat
@@ -1,1 +1,5 @@
-pyinstaller main.py --onefile --noconsole --icon=alt_death.ico --hidden-import=tkinter --dist ./ --name Double_Alt_Death
+@echo off
+pyinstaller main.py --onefile --noconsole --icon=alt_death.ico --hidden-import=tkinter --dist ./ --name Double_Alt_Death --clean
+rm ./__pycache__ -r
+rm ./build -r
+rm ./Double_Alt_Death.spec
