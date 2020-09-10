de62de734af3c8f614bfe44bf739fc7758f4555e
sevenc-nanashi <sevenc_nanashi@yahoo.co.jp>
2020-09-11 05:36:00 +0900

    specを無視するように

--- src/make.bat
+++ src/make.bat
@@ -2,3 +2,4 @@
 pyinstaller main.py --onefile --icon=alt_death.ico --hidden-import "tkinter" --hidden-import "tk.Tk" --clean --name Double_Alt_Death --dist ../ -w --hidden-import "wx"
 rm ./__pycache__ -r
 rm ./build -r
+rm ./Double_Alt_Death.specS
