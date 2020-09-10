6b2ba89034731cc2762ecccc16a891f39e3bbef1
sevenc-nanashi <sevenc_nanashi@yahoo.co.jp>
2020-09-11 05:32:33 +0900

    タスクトレイ表示の追加

--- src/make.bat
+++ src/make.bat
@@ -1,4 +1,4 @@
 @echo off
-pyinstaller main.py --onefile --icon=alt_death.ico --hidden-import "tkinter" --hidden-import "tk.Tk" --clean --name Double_Alt_Death --dist ../ -w
+pyinstaller main.py --onefile --icon=alt_death.ico --hidden-import "tkinter" --hidden-import "tk.Tk" --clean --name Double_Alt_Death --dist ../ -w --hidden-import "wx"
 rm ./__pycache__ -r
 rm ./build -r
